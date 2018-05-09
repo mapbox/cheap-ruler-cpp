0960306221661597f89f428d2609d5173444ec9d
Charles Karney <charles.karney@sri.com>
2018-05-09 04:26:24 -0400

    long_diff renamed to longDiff.

--- include/mapbox/cheap_ruler.hpp
+++ include/mapbox/cheap_ruler.hpp
@@ -86,7 +86,7 @@ public:
     // Given two points of the form [x = longitude, y = latitude], returns the distance.
     //
     double distance(point a, point b) {
-        auto dx = long_diff(a.x, b.x) * kx;
+        auto dx = longDiff(a.x, b.x) * kx;
         auto dy = (a.y - b.y) * ky;
 
         // Equivalent to (but faster than) std::hypot(dx, dy)
@@ -97,7 +97,7 @@ public:
     // Returns the bearing between two points in angles.
     //
     double bearing(point a, point b) {
-        auto dx = long_diff(b.x, a.x) * kx;
+        auto dx = longDiff(b.x, a.x) * kx;
         auto dy = (b.y - a.y) * ky;
 
         return std::atan2(dx, dy) * 180. / M_PI;
@@ -143,7 +143,7 @@ public:
             auto& ring = poly[i];
 
             for (unsigned j = 0, len = ring.size(), k = len - 1; j < len; k = j++) {
-                sum += long_diff(ring[j].x, ring[k].x) *
+                sum += longDiff(ring[j].x, ring[k].x) *
                   (ring[j].y + ring[k].y) * (i ? -1. : 1.);
             }
         }
@@ -189,11 +189,11 @@ public:
             auto t = 0.;
             auto x = line[i].x;
             auto y = line[i].y;
-            auto dx = long_diff(line[i + 1].x, x) * kx;
+            auto dx = longDiff(line[i + 1].x, x) * kx;
             auto dy = (line[i + 1].y - y) * ky;
 
             if (dx != 0. || dy != 0.) {
-                t = (long_diff(p.x, x) * kx * dx +
+                t = (longDiff(p.x, x) * kx * dx +
                      (p.y - y) * ky * dy) / (dx * dx + dy * dy);
                 if (t > 1) {
                     x = line[i + 1].x;
@@ -205,7 +205,7 @@ public:
                 }
             }
 
-            dx = long_diff(p.x, x) * kx;
+            dx = longDiff(p.x, x) * kx;
             dy = (p.y - y) * ky;
 
             auto sqDist = dx * dx + dy * dy;
@@ -326,12 +326,12 @@ public:
     bool insideBBox(point p, box bbox) {
         return p.y >= bbox.min.y &&
                p.y <= bbox.max.y &&
-               long_diff(p.x, bbox.min.x) >= 0 &&
-               long_diff(p.x, bbox.max.x) <= 0;
+               longDiff(p.x, bbox.min.x) >= 0 &&
+               longDiff(p.x, bbox.max.x) <= 0;
     }
 
     static point interpolate(point a, point b, double t) {
-        double dx = long_diff(b.x, a.x);
+        double dx = longDiff(b.x, a.x);
         double dy = b.y - a.y;
 
         return point(a.x + dx * t, a.y + dy * t);
@@ -340,7 +340,7 @@ public:
 private:
     double ky;
     double kx;
-    static double long_diff(double a, double b) {
+    static double longDiff(double a, double b) {
         return std::remainder(a - b, 360);
     }
 };
